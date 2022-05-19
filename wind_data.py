7e932ae545dc4918c74f20972759184021638363
cnmorelli <91206562+cnmorelli@users.noreply.github.com>
2022-05-19 13:18:38 -0700

    Add files via upload

---
+++ wind_data.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import nsrdb_weather as ns
+lat = 39.124227
+long = -121.066468
+
+data = ns.getyear(2020,lat,long)
+df = pd.concat(data['DataFrame'])
+
+# read in date, time, month, day
+df.insert(0, 'DateTime', df.index)
+df.insert(1, 'Date', pd.to_datetime(df['DateTime']).dt.date)
+df.insert(2, 'Time', pd.to_datetime(df['DateTime']).dt.time)
+df.insert(3, 'Month', pd.DatetimeIndex(df['Date']).month)
+df.insert(4, 'Day', pd.DatetimeIndex(df['Date']).day)
+df.insert(5, 'Year', 2020)
+
+# average wind speed by day
+df_new = df.groupby(pd.Grouper(key='Date')).mean() # change to max?
+df_new.insert(0, 'Date', df_new.index)
+
+PGE_2020 = pd.read_csv('PGE_2020.csv')
+PGE_2020.insert(6, 'Wind Speed', 0)
+PGE_2020 = PGE_2020[['Date', 'Year', 'Month', 'Day', 'Time', 'Latitude', 'Longitude', 'Wind Speed']]
+df_date = PGE_2020[['Month', 'Day', 'Year']]
+PGE_2020['Date']= pd.to_datetime(df_date)
+
+for i in PGE_2020.index:
+    lat = PGE_2020['Latitude'][i]
+    long = PGE_2020['Longitude'][i]
+    date = PGE_2020['Date'][i]
+    # need to match date to date in df_new and get wind speed
+    #wind_speed = df_new.loc[date, 'wind_speed[m/s]'] ??????????
+
+
+    #search lat and long in API
+
+    #wind_speed = API(date, lat, long)
+    #PGE_2021['Wind Speed'][i] = wind_speed
+
+
+
